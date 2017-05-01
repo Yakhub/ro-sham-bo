5070f39fab99a7f7f105c106fb6c446d192a2cdf
yakub pasha shaik <yakhubpasha@gmail.com>
2017-05-01 12:33:56 +0530

    Initital check in..

---
+++ src/main/java/org/game/microservices/RoShamBoApplication.java
@@ -0,0 +1,12 @@
+package org.game.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoShamBoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoShamBoApplication.class, args);
+	}
+}
---
+++ src/test/java/org/game/microservices/RoShamBoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.game.microservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoShamBoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
